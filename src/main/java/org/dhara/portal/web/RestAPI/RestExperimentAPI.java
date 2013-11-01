75a59e3f6f7112dbe868dc94a237419a6b43ea8a
harsha89 <harsz89@gmail.com>
2013-11-01 17:20:34 +0530

    rest service minor changes

--- src/main/java/org/dhara/portal/web/RestAPI/RestExperimentAPI.java
+++ src/main/java/org/dhara/portal/web/RestAPI/RestExperimentAPI.java
@@ -2,6 +2,7 @@ package org.dhara.portal.web.RestAPI;
 
 import org.apache.airavata.registry.api.workflow.ExperimentData;
 import org.dhara.portal.web.airavataService.AiravataClientAPIService;
+import org.dhara.portal.web.helper.ExperimentHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -31,17 +33,32 @@ public class RestExperimentAPI {
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public List<ExperimentData> handleRequestInternal(Model model, HttpServletRequest httpServletRequest, HttpServletResponse response) throws Exception {
+    public List<ExperimentHelper> handleRequestInternal(Model model, HttpServletRequest httpServletRequest, HttpServletResponse response) throws Exception {
 
         List<ExperimentData> experimentData = airavataAPIService.getExperimentData();
+        List<ExperimentHelper> experimentHelpers = new ArrayList<ExperimentHelper>();
+        for (int index = 0; index < experimentData.size(); index++) {
+            ExperimentHelper helper = new ExperimentHelper();
+            ExperimentData data = experimentData.get(index);
+            helper.setName(data.getExperimentName());
+            helper.setUpdatedTime(data.getStatusUpdateTime());
+            helper.setAuthor(data.getUser());
+            helper.setState(data.getState().toString());
+            //retrieve experiment data by id
+           /* List<NodeExecutionData> nodeExecutionDataList = airavataClientAPIService.getWorkflowExperimentData(data.getExperimentId());
+            List<Nodehelper> nodehelperList = new ArrayList<Nodehelper>();
 
-        Map paramMap = WebUtils.getParametersStartingWith(httpServletRequest, "d-");
-        if (paramMap.size() == 0) {
-            WebUtils.setSessionAttribute(httpServletRequest, "experiments", experimentData);
+            for (int i = 0; i < nodeExecutionDataList.size(); i++) {
+                Nodehelper nodehelper = new Nodehelper();
+                nodehelper.setType(nodeExecutionDataList.get(i).getType().name());
+                nodehelper.setInput(nodeExecutionDataList.get(i).getInput());
+                nodehelper.setOutput(nodeExecutionDataList.get(i).getOutput());
+                nodehelper.setWorkflowInstanceNodeId(nodeExecutionDataList.get(i).getWorkflowInstanceNode().getNodeId());
+                nodehelperList.add(nodehelper);
+            }
+            helper.setNodehelperList(nodehelperList);*/
+            experimentHelpers.add(helper);
         }
-        model.addAttribute("data", experimentData);
-
-
-        return experimentData;
+        return experimentHelpers;
     }
 }
